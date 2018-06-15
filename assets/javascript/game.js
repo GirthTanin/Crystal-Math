027ee2756d573868a37ae8ebe62a06cecbcabdf7
Jonathan McCarthy <JonathanFMcCarthy@gmail.com>
2018-06-14 21:34:04 -0400

    it's working except the restart

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,6 +3,7 @@ $(document).ready(function() {
     console.log("document loaded");
 
 
+
 // this next bit keeps the space bar from auto scrolling
 window.onkeydown = function(e) {
     if (e.keyCode == 32 && e.target == document.body) {
@@ -11,20 +12,25 @@ window.onkeydown = function(e) {
     }
 };
 
+//this is where the real stuff begins
 var counter1 = 0;
 $("#counter").text(counter1);
+
 
 //this will keep track of wins and losses
 var wins = 0;
 var losses = 0;
 
-$("#wins").html("&nbspWins: " + wins+"&nbsp");
-$("#losses").html("&nbspLosses: " + losses +"&nbsp");
+$("#wins").html("&nbspWins: " + wins + "&nbsp");
+$("#losses").html("&nbspLosses: " + losses + "&nbsp");
+
 
 var start1 = document.getElementById("start");
 var directions1 = document.getElementById("directions");
 
 console.log("game.js connected");
+
+
 
 //this is all the random math generators
 var randomGameNum = Math.floor(Math.random() * 101+19);
@@ -38,28 +44,18 @@ console.log(randomPurpleNum);
 var randomGreenNum  = Math.floor(Math.random() * 11+1);
 console.log(randomGreenNum);
 
-// this will set up the winner or loser functions to be called later.
-function winner() {
-    $("#youDidSomething").text("WINNER WINNER CHICKEN DINNER");
-    console.log("youDidSomething worked");
-    wins ++;
-    $("#wins").text(wins);
-    start();
-}
-function loser() {
-    $("#youDidSomething").text("It can't rain all the time!");
-    console.log("youDidSomething worked");
-    losses ++;
-    $("#losses").text(losses);
-    start();
-}
 
 
+
+function fiveSeconds() {
+    $("#youDidSomething").text("");
+    console.log("timer worked");
+}
 
 
 
 // this should be the first called function from the onclick event
-function start() {
+function goodTry() {
     $("#numberDemanded").text(randomGameNum);
 
     if (start1.style.display === "block") {
@@ -73,17 +69,18 @@ function start() {
     }
 
     //this is all the random math generators
-// var randomGameNum = Math.floor(Math.random() * 101+19);
-// console.log(randomGameNum);
-// var randomBlueNum = Math.floor(Math.random() * 11+1);
-// console.log(randomBlueNum);
-// var randomWhiteNum = Math.floor(Math.random() * 11+1);
-// console.log(randomWhiteNum);
-// var randomPurpleNum = Math.floor(Math.random() * 11+1);
-// console.log(randomPurpleNum);
-// var randomGreenNum  = Math.floor(Math.random() * 11+1);
-// console.log(randomGreenNum);
+        var randomGameNum = Math.floor(Math.random() * 101+19);
+        console.log(randomGameNum);
+        var randomBlueNum = Math.floor(Math.random() * 11+1);
+        console.log(randomBlueNum);
+        var randomWhiteNum = Math.floor(Math.random() * 11+1);
+        console.log(randomWhiteNum);
+        var randomPurpleNum = Math.floor(Math.random() * 11+1);
+        console.log(randomPurpleNum);
+        var randomGreenNum  = Math.floor(Math.random() * 11+1);
+        console.log(randomGreenNum);
 
+}
 
 
     $("#crystalBlue").on("click", function(){
@@ -138,10 +135,30 @@ function start() {
     });    
     
 
+// this will set up the winner or loser functions to be called later.
 
+function winner() {
+    $("#youDidSomething").text("WINNER WINNER CHICKEN DINNER");
+    console.log("youDidSomething worked");
+    wins ++;
+    $("#wins").html("&nbspWins: " + wins + "&nbsp");
+    setTimeout(fiveSeconds, 1000 * 5);
+    goodTry;
+}
+
+function loser() {
+    $("#youDidSomething").text("It can't rain all the time!");
+    console.log("youDidSomething worked");
+    losses ++;
+    $("#losses").html("&nbspLosses: " + losses + "&nbsp");
+    setTimeout(fiveSeconds, 1000 * 5);
+    goodTry;
 }
 
 
+
+// $("#wins").html("&nbspWins: " + wins + "&nbsp");
+// $("#losses").html("&nbspLosses: " + losses + "&nbsp");
 
 
 
@@ -168,6 +185,5 @@ document.onclick = function(event) {
         
     // }
 
-    start();
 };
 });
